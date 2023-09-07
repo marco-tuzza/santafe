067ae0cd80253d566fcb71eba6917cc498d52009
Marco Tuzza <marcotuzza@hotmail.it>
2023-09-07 16:59:43 +0200

    Hero page first version

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-
+<section class="container" id="hero-section">
+    <div id="hero-section-container">
+        <div id="hero-section-title-container">
+            <h2>
+                <span class="hero-section-title">Elevating Tastes,</span>
+                <span class="hero-section-title">Igniting Delight</span>
+            </h2>
+            <button class="btn btn-primary sf-primary-button">
+                <a href="/menu" title="View our menu">Browse Menu</a>
+            </button>
+        </div>
+        <div id="hero-section-circle">
+            <img src="{{ asset('images/santafe_circle.png')}}" alt="">
+        </div>
+    </div>
+</section>
 @endsection
