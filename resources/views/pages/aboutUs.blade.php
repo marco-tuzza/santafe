c26c9cb682af571a4d24d7434a8c9213cc920a4b
Marco Tuzza <marco.tuzza@capgemini.com>
2023-09-19 13:40:54 +0200

    About us desktop completed

---
+++ resources/views/pages/aboutUs.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="about-us-title-container">
+    <h1>
+        About Us
+    </h1>
+</div>
+<div class="container" id="about-us-content-container">
+    <div class="about-us-content-text">
+        <p>
+            <span class="styled-capital-letter">W</span>elcome to Santafe, one of 
+            Yerevan's finest open-air café-restaurants, proudly owned by the 
+            renowned Galaxy Group of Companies. With a legacy dating back to 2005, 
+            Santafe stands as a beloved fixture in Yerevan's vibrant culinary 
+            landscape, offering a unique fusion of entertainment and hospitality.
+        </p>
+    </div>
+
+    <div class="about-us-content-text-image-container">
+        <div class="about-us-content-text">
+            <h2 class=about-us-content-text-title>
+                A Legacy of Excellence in Entertainment & Hospitality
+            </h2>
+            <p>
+                Santafe is synonymous with excellence, and our commitment 
+                to delivering exceptional experiences is embedded in every 
+                facet of our establishment. Boasting a dynamic team of 120 
+                dedicated professionals, our staff hails from diverse culinary 
+                backgrounds and countries, ensuring a rich tapestry of culinary 
+                expertise at your service.
+            </p>
+        </div>
+        <div class="about-us-content-image">
+
+        </div>
+    </div>
+
+    <div class="about-us-content-text">
+        <h2 class=about-us-content-text-title>
+            Meet Our Visionary CEO: Tigran Soghomonyan
+        </h2>
+        <p>
+            Guided by the visionary leadership of our CEO, Tigran Soghomonyan, 
+            Santafe consistently pushes boundaries and redefines the café-restaurant
+            experience. Tigran's unwavering dedication to innovation and unwavering
+            pursuit of quality have charted our course towards becoming an 
+            unparalleled dining and entertainment destination.
+        </p>
+    </div>
+
+    <div class="about-us-content-text-image-container">
+        <div class="about-us-content-image">
+
+        </div>
+        <div class="about-us-content-text">
+            <h2 class=about-us-content-text-title>
+                Discover Santafe: Where Tradition Harmonizes with Innovation
+            </h2>
+            <p>
+                Ideally situated in Yerevan's central district, Santafe beckons 
+                you to explore its treasures, mere footsteps away from iconic 
+                landmarks such as the Cascade and Opera House. Our establishment 
+                proudly encapsulates the essence of tradition and innovation.
+            </p>
+        </div>
+    </div>
+
+    <div class="about-us-content-text">
+        <h2 class=about-us-content-text-title>
+            Products & Services
+        </h2>
+        <p>
+            At Santafe, we cater to diverse tastes and preferences, offering a 
+            range of products and services to enhance your visit: 
+            <ul class="about-us-content-text-list">
+                <li>
+                    <span class="about-us-content-text-list-element-title">Kids Zone:</span> 
+                    Families are welcomed with open arms, and our dedicated Kids Zone ensures
+                    young patrons enjoy a safe and entertaining experience. 
+                </li>
+                <li>
+                    <span class="about-us-content-text-list-element-title">HD Quality Sporting Event Broadcasts:</span> 
+                    Sports enthusiasts can immerse themselves in the thrill of the game with our 
+                    high-definition broadcasts, creating an electric atmosphere for fans of all ages.
+                </li>
+            </ul> 
+        </p>
+    </div>
+
+    <div class="about-us-content-text">
+        <h2 class=about-us-content-text-title>
+            Game-Changing Innovations
+        </h2>
+        <p>
+            Santafe has a history of trailblazing in café-restaurant design,
+            pioneering concepts such as:
+            <ul class="about-us-content-text-list">
+                <li>
+                    <span class="about-us-content-text-list-element-title">Ground Fountains:</span> 
+                    Our outdoor space is adorned with captivating ground fountains, 
+                    infusing a soothing ambiance that sets us apart. 
+                </li>
+                <li>
+                    <span class="about-us-content-text-list-element-title">Green Zone:</span> 
+                    Lush greenery seamlessly intertwines with our design, 
+                    providing a serene escape amidst the urban hustle and bustle.
+                </li>
+                <li>
+                    <span class="about-us-content-text-list-element-title">Wine Zone:</span> 
+                    Wine aficionados can savor an exquisite selection,
+                    adding an element of sophistication to their dining experience.
+                </li>
+                <li>
+                    <span class="about-us-content-text-list-element-title">Children’s Playground:</span> 
+                    Our dedication to family-friendly entertainment extends to a dedicated playground, 
+                    allowing families to dine and relax while children play.
+                </li>
+            </ul> 
+        </p>
+    </div>
+</div>
+@endsection('content')
