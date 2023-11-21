b35c3b82bd68d7057e49b74a076bbd4ea42d4b36
Lilit Bakhshetsyan <122392598+LilithBakh@users.noreply.github.com>
2023-11-21 17:19:49 +0400

    Fixed the errors and indentation

--- resources/js/components/contact-us.js
+++ resources/js/components/contact-us.js
@@ -1,81 +1,96 @@
-// import $ from 'jquery';
-
-/* REMEMBER TO USE JQUERYYYYYYYYYY */
+import $ from 'jquery';
 
 $(formValidation);
 
-function formValidation() {
-
+function formValidation() 
+{
     const form = $('#contact-us-form');
     const username = $('#name');
     const email = $('#email');
     const phone = $('#phone');
     const message = $('#message');
     const errorElement = $('#error');
- 
 
     form.on('submit', (e) => {
-        let messages = [];
+        let errorMessages = [];
         const nameRegex = /^[a-zA-Z\u00C0-\u00FF\s]+$/;
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         const phoneRegex = /^\d+$/;
          
-        nameConditions(username, nameRegex, messages);
-
-        emailConditions(email, emailRegex, messages);
-
-        phoneConditions(phone, phoneRegex, messages);
-
-        messageConditions(message, messages);
-
-        validationCheck(e, messages, errorElement);
-
-
+        nameConditions(username, nameRegex, errorMessages);
+        emailConditions(email, emailRegex, errorMessages);
+        phoneConditions(phone, phoneRegex, errorMessages);
+        messageConditions(message, errorMessages);
+        validationCheck(e, errorMessages, errorElement);
     });
 }
 
-function nameConditions(username, nameRegex, messages) {
-    if (!nameRegex.test(username.val())) {
-        messages.push('Name is invalid. It should only contain letters and spaces, and it cannot be empty.');
-    } else if (username.val().length < 2){
-        messages.push('Name must be at least 2 characters long');
-    } else if (username.val().length > 255) {
-        message.push('Name must be less than 255 characters long');
+function nameConditions(username, nameRegex, errorMessages) 
+{
+    if (!nameRegex.test(username.val()))
+    {
+        errorMessages.push('Name is invalid. It should only contain letters and spaces, and it cannot be empty.');
+    } 
+    else if (username.val().length < 2)
+    {
+        errorMessages.push('Name must be at least 2 characters long');
+    } 
+    else if (username.val().length > 255) 
+    {
+        errorMessages.push('Name must be less than 255 characters long');
     }
 }
 
-function emailConditions(email, emailRegex, messages) {
-    if (!emailRegex.test(email.val())) {
-        messages.push('Please enter a valid email address');
-    } else if (email.val().length < 5) {
-        messages.push('Email must be at least 5 characters long');
-    } else if (email.val().length > 255) {
-        messages.push('Email must be less than 255 characters long');
+function emailConditions(email, emailRegex, errorMessages) 
+{
+    if (!emailRegex.test(email.val())) 
+    {
+        errorMessages.push('Please enter a valid email address');
+    } 
+    else if (email.val().length < 5) 
+    {
+        errorMessages.push('Email must be at least 5 characters long');
+    } 
+    else if (email.val().length > 255) 
+    {
+        errorMessages.push('Email must be less than 255 characters long');
     }
 }
 
-function phoneConditions(phone, phoneRegex, messages) {
-    if (!phoneRegex.test(phone.val())) {
-        messages.push('Phone number is invalid. It should only contain numbers, and it cannot be empty.');
-    } else if (phone.val().length < 4) {
-        messages.push('Phone number must be at least 4 characters long');
-    } else if (phone.val().length > 16) {
-        messages.push('Phone number must be less than 16 characters long');
+function phoneConditions(phone, phoneRegex, errorMessages) 
+{
+    if (!phoneRegex.test(phone.val())) 
+    {
+        errorMessages.push('Phone number is invalid. It should only contain numbers, and it cannot be empty.');
+    } 
+    else if (phone.val().length < 4) 
+    {
+        errorMessages.push('Phone number must be at least 4 characters long');
+    } 
+    else if (phone.val().length > 16) 
+    {
+        errorMessages.push('Phone number must be less than 16 characters long');
     }
 }
 
-function messageConditions(message, messages) {
-    if (message.val().length < 15) {
-        messages.push('Message must be at least 15 characters long');
-    } else if (message.val().length > 1000) {
-        messages.push('Message must be less than 1000 characters long');
+function messageConditions(message, errorMessages) 
+{
+    if (message.val().length < 15) 
+    {
+        errorMessages.push('Message must be at least 15 characters long');
+    } 
+    else if (message.val().length > 1000) 
+    {
+        errorMessages.push('Message must be less than 1000 characters long');
     }
 }
 
-function validationCheck(e, messages, errorElement) {
-    if (messages.length > 0) {
+function validationCheck(e, errorMessages, errorElement) 
+{
+    if (errorMessages.length > 0) 
+    {
         e.preventDefault();
-        errorElement.html(messages.map(msg => `<div>${msg}</div>`).join(''));
+        errorElement.html(errorMessages.map(msg => `<div>${msg}</div>`).join(''));
     }
 
 }
